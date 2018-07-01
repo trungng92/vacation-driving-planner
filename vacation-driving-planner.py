c27b304faadeaf197eaff6f6658e633546f071db
Trung Nguyen <trungng92@gmail.com>
2018-07-01 14:42:29 -0400

    Fill out stub functions to actually make gmaps calls.

--- vacation-driving-planner.py
+++ vacation-driving-planner.py
@@ -11,7 +11,6 @@
 ### Deploy Django on aws https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/create-deploy-python-django.html
 ### Deploy Django on Heroku https://devcenter.heroku.com/articles/getting-started-with-python#introduction
 
-import datetime
 import pprint
 from typing import Callable, List
 import os
@@ -20,54 +19,75 @@ import googlemaps
 
 API_KEY = os.environ['GOOGLE_MAPS_API_KEY']
 
-gmaps = googlemaps.Client(key=API_KEY)
-
-now = datetime.date
-directions_result = gmaps.directions("Sydney Town Hall",
-                                     "Parramatta, NSW",
-                                     mode="driving")
-print(directions_result)
-pp = pprint.pprint(directions_result)
 
 class VacationDrivingPlanner:
     def __init__(self):
         self.driving_filter = DrivingFilter()
-
-    def get_destination_cities(self):
-        pass
+        self.origin_cities = OriginCities()
+        self.destination_cities = DestinationCities()
 
     def get_origin_cities(self):
-        pass
+        return self.origin_cities.get_cities()
 
-    def get_driving_distances(self, origin_cities: 'OriginCities', destination_cities: 'DestinationCities') -> List:
-        pass
+    def get_destination_cities(self):
+        return self.destination_cities.get_cities()
+
+    def get_driving_distances(self, origin_cities: List[str], destination_cities: List[str]) -> List:
+        driving_results = []
+
+        gmaps = googlemaps.Client(key=API_KEY)
+        for origin_city in origin_cities:
+            for destination_city in destination_cities:
+                directions_result = gmaps.directions(origin_city, destination_city, mode='driving')
+                driving_results.append(directions_result)
+
+        return driving_results
 
     def filter_driving_results(self, driving_results: List) -> List:
-        pass
+        return self.driving_filter.run(driving_results)
 
     def map_driving_results(self, driving_results: List):
         pass
 
-class DestinationCities:
-    def get_cities(self):
-        pass
+    def run(self):
+        origin_cities = self.get_origin_cities()
+        destination_cities = self.get_destination_cities()
 
-    def _get_cities_from_csv(self):
-        pass
+        initial_driving_results = self.get_driving_distances(origin_cities, destination_cities)
+        final_driving_results = self.filter_driving_results(initial_driving_results)
+
+        pprint.pprint(final_driving_results)
+
 
 class OriginCities:
-    def get_cities(self):
-        pass
+    def get_cities(self) -> List[str]:
+        return self._get_cities_default()
 
     def _get_cities_from_csv(self):
         pass
+
+    def _get_cities_default(self):
+        return ['Virginia Beach, Virginia', 'Atlanta, Georgia']
+
+
+class DestinationCities:
+    def get_cities(self) -> List[str]:
+        return self._get_cities_default()
+
+    def _get_cities_from_csv(self):
+        pass
+
+    def _get_cities_default(self):
+        return ['Lakeland, Florida', 'Cary, North Carolina']
+
 
 class DrivingFilter:
     def __init__(self):
         self.filters = {}
 
     def add_filter(self, filter_name: str, filter_func: Callable[[List], List]):
-        pass
+        """Note: You can also override previous filters with this"""
+        self.filters[filter_name] = filter_func
 
     def remove_filter(self, filter_name: str):
         delattr(self.filters, filter_name)
@@ -78,3 +98,8 @@ class DrivingFilter:
             driving_results = filter_func(driving_results)
 
         return driving_results
+
+
+if __name__ == '__main__':
+    vdp = VacationDrivingPlanner()
+    vdp.run()
